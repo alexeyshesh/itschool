93b2ad7c7828cf992c96b0170178797e0085d7a7
Alexey Sheshukov <alexeyshesh@yandex.ru>
2021-11-24 00:05:52 +0300

    Занятие 30

---
+++ Занятие 30/main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from social_network import settings
+
+from . import views
+
+urlpatterns = [
+    path('logout', views.logout),
+    path('reg', views.reg),
+    path('login', views.login),
+    path('subscribes/<str:username>/<str:action>', views.subscribes),
+    path('news', views.news),
+    path('settings', views.user_settings),
+    path('media/<str:filename>', views.image),
+    path('<str:username>', views.user),
+    path('', views.index),
+]
